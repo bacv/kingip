68418e7a92d1201a8af2c792deef06238477f2f4
Gusto <bacvinka@gmail.com>
2024-02-09 14:31:24 +0200

    Extract quic listener and dialer to lib

---
+++ lib/quic/dialer.go
@@ -0,0 +1,92 @@
+package quic
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+
+	proto "github.com/bacv/kingip/lib/proto"
+	"github.com/quic-go/quic-go"
+)
+
+type DialerStreamHandleFunc func(quic.Stream) error
+
+type DialerConfig struct {
+	Addr    net.Addr
+	Regions map[string]string
+}
+
+type Dialer struct {
+	config        DialerConfig
+	streamHandler DialerStreamHandleFunc
+}
+
+func NewDialer(
+	config DialerConfig,
+	streamHandler DialerStreamHandleFunc,
+) *Dialer {
+	return &Dialer{
+		config:        config,
+		streamHandler: streamHandler,
+	}
+}
+
+func (s *Dialer) Dial(ctx context.Context) error {
+	conn, err := quic.DialAddrEarly(
+		ctx, s.config.Addr.String(),
+		TlsClientConfig.Clone(),
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	configStream, err := conn.OpenStream()
+	if err != nil {
+		return err
+	}
+
+	transport, err := SyncTransport(
+		configStream,
+		s.handleConfig,
+		proto.NewMsgRelayHello(s.config.Regions),
+	)
+	transport.Close()
+
+	if err != nil {
+		return err
+	}
+
+	// Listen for new streams comming from the server.
+	return s.listenStreams(conn)
+}
+
+func (s *Dialer) handleConfig(w ResponseWriter, r proto.Message) error {
+	mt, id, err := r.UnmarshalString()
+	if err != nil {
+		return err
+	}
+
+	if proto.MsgRelayConfig != mt {
+		errors.New("Wrong protocol message")
+	}
+
+	log.Println("Got id from stream: ", id)
+	return nil
+}
+
+func (s *Dialer) listenStreams(conn quic.Connection) error {
+	for {
+		stream, err := conn.AcceptStream(context.Background())
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			if err := s.streamHandler(stream); err != nil {
+				log.Print(err)
+			}
+		}()
+	}
+}
---
+++ lib/quic/listener.go
@@ -0,0 +1,104 @@
+package quic
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	proto "github.com/bacv/kingip/lib/proto"
+	"github.com/quic-go/quic-go"
+)
+
+type ListenerRegisterHandleFunc func(quic.Connection) (uint64, <-chan error, error)
+type ListenerRegionsHandleFunc func(uint64, map[string]string) error
+
+type ListenerConfig struct {
+	Addr net.Addr
+}
+
+type Listener struct {
+	config          ListenerConfig
+	registerHandler ListenerRegisterHandleFunc
+	regionsHandler  ListenerRegionsHandleFunc
+}
+
+func NewListener(
+	ctx context.Context,
+	config ListenerConfig,
+	registerHandler ListenerRegisterHandleFunc,
+	regionsHandler ListenerRegionsHandleFunc,
+) *Listener {
+	return &Listener{
+		config:          config,
+		registerHandler: registerHandler,
+		regionsHandler:  regionsHandler,
+	}
+}
+
+func (s *Listener) Listen() error {
+	listener, err := quic.ListenAddr(s.config.Addr.String(), GenerateTLSConfig(), nil)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := listener.Accept(context.Background())
+		if err != nil {
+			log.Println("Unable to accept conn: ", err)
+			continue
+		}
+
+		go func() {
+			err := s.handleConn(conn)
+			if err != nil {
+				log.Println("Failed to handle conn: ", err)
+			}
+		}()
+	}
+}
+
+func (s *Listener) handleConn(conn quic.Connection) error {
+	for {
+		helloStream, err := conn.AcceptStream(context.Background())
+		if err != nil {
+			return err
+		}
+
+		id, stopC, err := s.registerHandler(conn)
+		if err != nil {
+			return err
+		}
+
+		// Receive first stream from dialer.
+		if err := s.handleHelloStream(id, helloStream); err != nil && err != io.EOF {
+			return err
+		}
+
+		// Wait until handler finishes.
+		return <-stopC
+	}
+}
+
+func (s *Listener) handleHelloStream(id uint64, stream quic.Stream) error {
+	handleHello := func(w ResponseWriter, r proto.Message) error {
+		mt, regions, err := r.UnmarshalMap()
+		if err != nil {
+			return err
+		}
+
+		if proto.MsgRelayHello != mt {
+			log.Println("Wrong protocol message")
+		}
+
+		s.regionsHandler(id, regions)
+		log.Println("Got id with regions: ", regions)
+		w.Write(proto.NewMsgRelayConfig(fmt.Sprint(id)))
+		return nil
+	}
+
+	transport := NewTransport(stream, handleHello)
+	defer transport.Abandon()
+
+	return transport.Spawn()
+}
